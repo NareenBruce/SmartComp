579098b08da8b731f0fa0fd4bf16876670a6d931
Nareenthiran <nareenthira5@gmail.com>
2024-12-15 15:19:21 +0800

    new

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ app= Flask(__name__)
 conn = sqlite3.connect('project.db')
 cursor = conn.cursor()
 
+def create_table():
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS login (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect('project.db')
@@ -17,12 +28,19 @@ def close_db(error):
         db.close()
 
 @app.route("/")
-def home(): 
+def home():
     return render_template("main.html")
 
 @app.route("/login")
-def login():
+def login(username, password):
     return render_template("login.html")
+
+def loginauth(username, password):
+    cursor.execute('SELECT * FROM login WHERE username = ? AND password = ?', (username, password))
+    if cursor.fetchone() is not None:
+        return render_template("homepage.html")
+    else:
+        "Invalid username or password"
 
 @app.route("/signup")
 def signup():
@@ -33,7 +51,10 @@ def superadmin():
     return render_template("sa_homepage.html")
 
 @app.route("/usersignup")
-def usersignup():
+def usersignup(username, password):
+    cursor.execute('INSERT INTO login (username, password) VALUES (?, ?)', (username, password))
+    conn.commit()
+    conn.close()
     return render_template("u_signup.html")
 
 @app.route("/caresignup")
@@ -41,4 +62,5 @@ def caresignup():
     return render_template("c_signup.html")
 
 if __name__ == "__main__":
+    create_table()
     app.run(debug=True)
